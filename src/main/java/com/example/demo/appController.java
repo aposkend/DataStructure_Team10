403bf5fb1b71313864ee16466c3968d1f11af629
RexRed <a123ggg09@gmail.com>
2022-11-20 13:05:40 +0800

    Add files via upload

--- src/main/java/com/example/demo/appController.java
+++ src/main/java/com/example/demo/appController.java
@@ -28,6 +28,8 @@ public class appController {
 
     @GetMapping("/hello/{input}")
     public String hello(@PathVariable String input, Model model) {
+        initialize();
+        resultList = new ArrayList<WebTree>();
         HashMap<String,String> name;
         name = googleSearch(input);
         if(buildTree(name)){
@@ -46,35 +48,41 @@ public class appController {
         for(String web : websites.keySet()){
             WebPage p = new WebPage(websites.get(web),web);
             try {
-                p.setScore(this.keys.arraylist());
-            } catch (IOException e) {
+                p.setScore(this.keys);
+            } catch (IOException e1) {
                 // TODO Auto-generated catch block
-                e.printStackTrace();
+                e1.printStackTrace();
             }
             WebTree t = new WebTree(p);
             //Query
-            System.out.println(websites.get(web));
+            System.out.println("first Layer: "+websites.get(web));
+
             HashMap <String,String>websites2 = doQuery(websites.get(web));
             if(websites2 != null){
                 for(String url2: websites2.keySet()){
-                    WebPage p2 = new WebPage(websites2.get(url2), url2);
+                    if(url2.equals(websites.get(web)))continue;
+                    WebPage p2 = new WebPage(url2, websites2.get(url2));
+                    System.out.println("Second Layer: "+ url2);
                     try {
-                        p2.setScore(this.keys.arraylist());
+                        p2.setScore(this.keys);
+                        System.out.println("setScore of " + url2);
                     } catch (IOException e) {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
-                    WebTree t2 = new WebTree(p2);
-                    t.root.addChild(t2.root);
+                    WebNode node2 = new WebNode(p2);
+                    t.root.addChild(node2);
                 }
             }
+
             //Score
             try{
-              t.setPostOrderScore(t.root);
+              t.setPostOrderScore(this.keys);
               resultList.add(t);
             }catch(IOException e) {
               e.printStackTrace();
-            }
+           }
+            System.out.println("next round");
         }
     }   
         return true;
@@ -99,12 +107,11 @@ public class appController {
     }
     
     public HashMap<String,String> doQuery(String url){
-        GoogleQuery googleQuery = new GoogleQuery(null);
+        Query Query = new Query(url);
         try {
-            return googleQuery.searchHTML(url);
+            return Query.query();
         } catch (IOException e) {
-            // TODO Auto-generated catch block
-           // e.printStackTrace();
+            e.printStackTrace();
         }
         return null;
     }
@@ -121,109 +128,7 @@ public class appController {
     }
 
 
-    
-    public void addTree(HashMap<String,String> website){
-        for(String url : website.keySet()){
-            WebPage root = new WebPage(url, website.get(url));		
-			WebTree tree = new WebTree(root);
-            score(tree);
-        }
-    }
-
-    public void addNode(WebTree tree){
-        /* 
-        GoogleQuery google = new GoogleQuery(null);
-        WebNode root = tree.root;
-        int size = root.children.isEmpty() ? 0: root.children.size();
-        while(!root.children.isEmpty()){
-            root = root.children.get(3){
-
-            }
-        }
-        do{
-        try {
-            HashMap<String, String> result = google.searchHTML(root.webPage.url);
-            for(String url: result.keySet()){
-               WebNode node = new WebNode(new WebPage(url, result.get(url)));
-               root.children.add(node);
-               node.parent = root;
-               tree.count++;
-            }
-        } catch (IOException e) {
-            System.out.println("no result");
-        }
-    }while(size != 0);
-    */
-    }
-
-
-    public void score(WebTree tree){
-      //  WordCounter wordCounter = new WordCounter(tree.root.webPage.url);
-        try {
-            tree.root.webPage.setScore(this.keys.arraylist());
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        }
-/* 
-        try {
-            
-           // tree.setPostOrderScore(this.keys);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-*/
-
-    private void initialize(){
-        this.keys.add(new Keyword("動畫",300));
-        this.keys.add(new Keyword("動畫",300));
-        this.keys.add(new Keyword("蠟筆小新",50));
-        this.keys.add(new Keyword("野原",50));
-        this.keys.add(new Keyword("新之助", 50));
-        this.keys.add(new Keyword("美冴",50));
-        this.keys.add(new Keyword("廣志",50));
-        this.keys.add(new Keyword("小白",40));
-        this.keys.add(new Keyword("風間",40));
-        this.keys.add(new Keyword("雙葉幼稚園", 40));
-        this.keys.add(new Keyword("臼井儀人", 30));
-        this.keys.add(new Keyword("實況主", -100));
+    private KeywordList initialize(){
+        return this.keys;
     }
 }
-
-/*
- * 
- * public WebNode contetQuery(String url) throws IOException{
-
-		if(content==null){
-			content= fetchContent();
-		}
-
-		WebPage page = new WebPage();
-        WebNode node = new WebNode(page);
-
-		this.url = url;
-		for(KeywordList:keys){
-		}
-		while(content.indexOf(keyword) != -1) {
-			content = content.substring(content.indexOf(keyword) + keyword.length() -1);
-			retVal++;
-		}	
-
-		}
-		return retVal;
-
-
-        public String startSearch(String input){
-        WordCounter wordCounter = new WordCounter("http://soslab.nccu.edu.tw/Welcome.html");
-        int count = 0;
-        try {
-            count = wordCounter.countKeyword(input);
-        } catch (IOException e) {
-        
-            e.printStackTrace();
-        }
-        return "" + String.valueOf(count);
-    }
- */
